6eb8f32cda7968d54d745953bf57db92f7b1f7e5
Natalya Krukova <k-natali2009@mail.ru>
2025-09-14 16:39:10 +0300

    changes in processing

--- src/processing.py
+++ src/processing.py
@@ -14,7 +14,11 @@ def filter_by_state(list_diction: Iterable[dict], state='EXECUTED') -> Iterable[
     return new_diction
 
 
-def sort_by_date(list_diction: Iterable[dict]):
+def sort_by_date(list_diction: Iterable[dict])-> Iterable[dict]:
+    """
+    Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате (date).
+    """
     sorted_list_data = sorted(list_diction, key=lambda x: x['date'], reverse=True)
     return sorted_list_data
 
